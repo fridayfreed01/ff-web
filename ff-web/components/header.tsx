1636784a922ebf3efbae49add3cd43596418614e
Seth Freitag <friday.freed01@gmail.com>
2023-02-01 12:43:35 -0600

    updated header

--- ff-web/components/header.tsx
+++ ff-web/components/header.tsx
@@ -8,13 +8,11 @@ export const Header = () => {
                 <img src="./fridayfreed.jpg" className="grid justify-right w-20 m-2 border border-black"/>
             <div className="flex items-center">
                 <div className="hidden ml-10 space-x-8 lg:block">
-                    <a href="#" className="text-base font-medium text-white hover:text-indigo-50"> Solutions </a>
+                    <a href="#" className="text-base font-medium text-white hover:text-indigo-50"> Games I'm Playing </a>
 
-                    <a href="#" className="text-base font-medium text-white hover:text-indigo-50"> Pricing </a>
+                    <a href="#" className="text-base font-medium text-white hover:text-indigo-50"> DevLog </a>
 
-                    <a href="#" className="text-base font-medium text-white hover:text-indigo-50"> Docs </a>
-
-                    <a href="#" className="text-base font-medium text-white hover:text-indigo-50"> Company </a>
+                    <a href="#" className="text-base font-medium text-white hover:text-indigo-50"> Links </a>
                 </div>
             </div>
             </nav>
