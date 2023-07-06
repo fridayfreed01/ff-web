7d7f03a27c8ed54470cd7fcdf45bb05256fbeba8
Seth Freitag <friday.freed01@gmail.com>
2023-07-06 18:11:47 -0500

    Favicon added

---
+++ ff-web/components/head.tsx
@@ -0,0 +1,11 @@
+import NextHead from "next/head";
+
+export function Head() {
+    return(
+        <NextHead>
+            <title>fridayfreed</title>
+            <link rel="icon" href="fridayfreed.jpg" />
+            <link rel="manifest" href="/manifest.json" />
+        </NextHead>
+    )
+}
--- ff-web/routes/aboutme.tsx
+++ ff-web/routes/aboutme.tsx
@@ -1,10 +1,12 @@
 import { About } from "../components/about";
 import { Footer } from "../components/footer";
+import { Head } from "../components/head";
 import { Header } from "../components/header";
 
 export const AboutMe = () => {
   return (
     <div>
+      <Head/>
       <Header />
       <About />
       <Footer />
